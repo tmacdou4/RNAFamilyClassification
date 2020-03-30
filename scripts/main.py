7edf23f31f20624b475be32003c2f3af8c3d0f4a
sauves <leonardsauve@gmail.com>
2020-03-30 13:48:55 -0400

    mv test_loader.py to utility.py changed ref in main, waiting for random seq generator

--- scripts/main.py
+++ scripts/main.py
@@ -1,17 +1,26 @@
 import pandas as pd
 import numpy as np
-from test_loader import *
+from utility import *
 import pdb
 import os
+import torch
 
 # global variables
 # paths
 datapath = 'data'
 # stats
-seq_len = 500 # how to get this number efficiently ?
+seq_len = 200 # how to get this number efficiently ?
 # Set RFs to include 
-RFs = ['RF00005', 'RF00009']
+RFs = ['RF00005', 'RF01852']
 # loading data into data frame
 data, labels = load_data_in_df(RFs, datapath = datapath, seq_len = seq_len)
 # debug
 pdb.set_trace()
+# train on 
+
+
+
+
+
+
+
