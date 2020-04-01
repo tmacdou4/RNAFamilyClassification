803c1b862bb18c247cbafbfed784e5a1e7cd4037
Tom MacDougall <thomasmmacdougall@gmail.com>
2020-04-01 10:07:51 -0300

    RNN working in models_test

--- scripts/models.py
+++ scripts/models.py
@@ -71,21 +71,63 @@ class CNN(nn.Module):
 class RNN(nn.Module):
     def __init__(self, model_specs):
         super(RNN, self).__init__()
-        self.emb_size = 2
-        self.embeddings = nn.Embedding(17, self.emb_size)
-        self.rnn = nn.RNN()
-        self.linear = nn.Linear()
+        self.batch_size = model_specs['batch_size']
+
+        # number of hidden layers
+        self.num_layers = model_specs['n_hid_lyrs']
+        self.non_linearity = nn.ReLU()
+        self.nt_vocab_size = 17
+        self.hidden_size = model_specs['HID1N']
+        self.out_size = model_specs['output_size']
+        self.bidirectional = False
+        self.dropout = 0
+
+        self.seq_len = model_specs['seq_len']  # this is eq to input size !
+        # Embbed or one-hot?
+        self.embed = False
+
+        if self.embed:
+            self.emb_size = 5
+            self.embeddings = nn.Embedding(17, self.emb_size)
+        else:
+            # one-hot encodings are effectively an embedding to space of 17
+            self.emb_size = 17
+
+        self.rnn = nn.RNN(self.emb_size, self.hidden_size, self.num_layers,
+                          batch_first=True, dropout=self.dropout, bidirectional=self.bidirectional)
+
+        if self.bidirectional:
+            self.out_layer = (nn.Linear(2*self.hidden_size, self.out_size))
+        else:
+            self.out_layer = (nn.Linear(self.hidden_size, self.out_size))
+
+        self.out_nl = nn.Softmax(dim=-1)
 
 
-    def forward(self, x):
-
-        if inputs.is_cuda:
-            device = inputs.get_device()
+    def forward(self, input):
+        if input.is_cuda:
+            device = input.get_device()
         else:
             device = torch.device("cpu")
 
-        embed_out = self.embeddings(inputs)
-        pass
+        if self.embed:
+            input = self.embeddings(input)
+        else:
+            input = one_hot_encoding(input)
+
+        _, input = self.rnn(input)
+
+        input = input.view(self.num_layers, int(self.bidirectional)+1, self.batch_size, self.hidden_size)
+
+        input = input[-1]
+        if self.bidirectional:
+            input = torch.cat((input[0], input[1]), dim=-1)
+
+        input=input.squeeze(0)
+        input = self.out_layer(input)
+        input = self.out_nl(input)
+
+        return input
 
 class LSTM(nn.Module):
     def __init__(self, model_specs):
