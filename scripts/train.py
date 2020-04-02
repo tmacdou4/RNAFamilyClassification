473d51c332a25ef0dd3b9b4da2b1da8d72b8fd6c
sauves <leonardsauve@gmail.com>
2020-04-02 18:34:42 -0400

    fixed reporters !

--- scripts/train.py
+++ scripts/train.py
@@ -27,17 +27,20 @@ def train (model, dataloader, model_specs, device = 'cuda:0', foldn = 0):
             y = Variable(y).to(device)
             out = model(x).to(device)
             n += 1
-            loss_val = loss(out[:,0], y).view(-1, 1).expand_as(out)
+            loss_val = loss(out[:,1], y).view(-1, 1).expand_as(out)
+            # loss, accuracy, and other performance metrics
             acc =torch.eq(out.argmax(dim = -1), y.long().flatten()).float().view(-1,1)
             a += float(acc.mean().detach().cpu().numpy())
             l += float(loss_val.mean())
             tn, fp, fn, tp = metrics.confusion_matrix(y.detach().cpu().numpy(), out.argmax(dim=-1).detach().cpu().numpy(), labels = np.arange(2, dtype=int)).ravel() 
+            yscores =  out[:,1].detach().cpu().numpy()
+            AUC = metrics.roc_auc_score(y_true = y.detach().cpu().numpy(), y_score = yscores)
+            # gradient steps 
             optimizer.zero_grad()
             loss_val.mean().backward()
             optimizer.step()
-            pdb.set_trace()
             #  print ('gradient step [ {} / {} ]'.format(n, model_specs['gr_steps']))
-            training_reporter = 'FOLD {} STEP[{}/{}]\tTRAIN ['.format(str(foldn).zfill(3), n ,model_specs['gr_steps'] ) + ''.join([['#','.'][int(j > int((i + 1.) * 10/epochs))] for j in range(10) ]) + '] [{}/{}] {} % '.format(i+1, epochs, round(a / n, 4) * 100)
+            training_reporter = 'FOLD {} STEP[{}/{}]\tTRAIN ['.format(str(foldn).zfill(3), n ,model_specs['gr_steps'] ) + ''.join([['#','.'][int(j > int((i + 1.) * 10/epochs))] for j in range(10) ]) + '] [{}/{}] acc = {} % | loss = {} | \t'.format(i+1, epochs, round(a / n, 4) * 100, round(l/n,3))
             CM_reporter = 'TP {}, TN {}, FP {} , FN {}, MCC : {}'.format(tp, tn, fp, fn, mcc)
             print(training_reporter + CM_reporter)
      
