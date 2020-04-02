2c32cd17dd63c0e332dfdcbb79f61b825debabb9
Tom MacDougall <thomasmmacdougall@gmail.com>
2020-04-02 19:30:21 -0300

    All models run in main.py

--- scripts/models_test.py
+++ scripts/models_test.py
@@ -10,7 +10,7 @@ from utility import *
 
 specs = {'n_hid_lyrs': 2, 'output_size' : 1, 'batch_size' : 50, 'HID1N': 8, 'seq_len': 300}
 
-net = DNN(specs).cuda()
+net = CNN(specs).cuda()
 
 #test sequences of shape (num_seq * seq_len) made of torch.long indexes
 sequences = torch.Tensor(np.random.randint(0, 17, size=(50, 300))).cuda()
