7b57bc25bec2c52573e1058c12d40f90f9a12f01
sauves <leonardsauve@gmail.com>
2020-04-03 16:38:19 -0400

    added some changes in TrainDNNs.py produced some results, everything is running

--- scripts/train.py
+++ scripts/train.py
@@ -9,40 +9,47 @@ def train (model, dataloader, model_specs, device = 'cuda:0', foldn = 0):
     wd = model_specs['wd']
     lr = model_specs['lr']
     loss = model_specs['lossfn']
+    gr_steps = model_specs['gr_steps']
     optimizer = torch.optim.Adam(model.parameters(), lr = lr, weight_decay = wd)
     # levels_sorted_by_index = sorted([(index, c) for (c, index) in model_specs['levels'].items()])
     # levels = [str(c)[:10] for (ind, c) in levels_sorted_by_index]
     frame_nb = 0
+    minibatch_count = 0
     for i  in range(epochs):
-        n = 0
+        n = 1
         l = 0
         a = 0
         m = 0 
         auc = 0
         TP, FN, FP, TN = 0,0,0,0
         mcc = 0
+        reporter_step = 100
         # CM = np.zeros((model_specs['output_size'], model_specs['output_size']))
         for x,y in dataloader:
             x = Variable(x).to(device)
             y = Variable(y).to(device)
+            # computational graph
             out = model(x).to(device)
-            n += 1
             loss_val = loss(out[:,1], y).view(-1, 1).expand_as(out)
             # loss, accuracy, and other performance metrics
-            acc =torch.eq(out.argmax(dim = -1), y.long().flatten()).float().view(-1,1)
-            a += float(acc.mean().detach().cpu().numpy())
-            l += float(loss_val.mean())
-            tn, fp, fn, tp = metrics.confusion_matrix(y.detach().cpu().numpy(), out.argmax(dim=-1).detach().cpu().numpy(), labels = np.arange(2, dtype=int)).ravel() 
-            yscores =  out[:,1].detach().cpu().numpy()
-            auc += metrics.roc_auc_score(y_true = y.detach().cpu().numpy(), y_score = yscores)
+            if minibatch_count % reporter_step== 0:
+                    n += 1
+                    acc =torch.eq(out.argmax(dim = -1), y.long().flatten()).float().view(-1,1)
+                    a += float(acc.mean().detach().cpu().numpy())
+                    l += float(loss_val.mean())
+                    tn, fp, fn, tp = metrics.confusion_matrix(y.detach().cpu().numpy(), out.argmax(dim=-1).detach().cpu().numpy(), labels = np.arange(2, dtype=int)).ravel() 
+                    yscores =  out[:,1].detach().cpu().numpy()
+                    auc += metrics.roc_auc_score(y_true = y.detach().cpu().numpy(), y_score = yscores)
+                    #  print ('gradient step [ {} / {} ]'.format(n, model_specs['gr_steps']))
+                    training_reporter = 'FOLD {} STEP[{}/{}]\tTRAIN ['.format(str(foldn).zfill(3), n ,model_specs['gr_steps'] ) + ''.join([['#','.'][int(j > int((i + 1.) * 10/epochs))] for j in range(10) ]) + '] [{}/{}] acc = {} % | loss = {} | AUC {} \t'.format(minibatch_count, gr_steps * epochs, round(a / n, 4) * 100, round(l/n,3), round(auc / n, 3))
+                    CM_reporter = 'TP {}, TN {}, FP {} , FN {}, MCC : {}'.format(tp, tn, fp, fn, mcc)
+                    print(training_reporter + CM_reporter)
             # gradient steps 
             optimizer.zero_grad()
+            # computational graph for efficiency
             loss_val.mean().backward()
             optimizer.step()
-            #  print ('gradient step [ {} / {} ]'.format(n, model_specs['gr_steps']))
-            training_reporter = 'FOLD {} STEP[{}/{}]\tTRAIN ['.format(str(foldn).zfill(3), n ,model_specs['gr_steps'] ) + ''.join([['#','.'][int(j > int((i + 1.) * 10/epochs))] for j in range(10) ]) + '] [{}/{}] acc = {} % | loss = {} | AUC {} \t'.format(i+1, epochs, round(a / n, 4) * 100, round(l/n,3), round(auc / n, 3))
-            CM_reporter = 'TP {}, TN {}, FP {} , FN {}, MCC : {}'.format(tp, tn, fp, fn, mcc)
-            print(training_reporter + CM_reporter)
+            minibatch_count += 1
     # update model_specs
     model_specs['tr_l'] = l / n
     model_specs['tr_acc'] = a / n
